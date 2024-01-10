f0c318f3949467cc2c666b1e6dbe97873b6a0e79
Justin Prim <justinprim140969@gmail.com>
2024-01-10 15:25:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("While loop for reference");
+            int i = 0; 
+            while (i<=5) 
+            {
+                Console.WriteLine(i);
+                i++;    
+            }    
+            Console.WriteLine("The for loop");        
+            for ( i = 0; i <=5; i++) {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
